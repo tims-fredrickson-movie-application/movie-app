e03eb3e9075444bb8618d2455fb49ffd7644fe0e
Stephen Fredrickson <stephen.fredrickson@gmail.com>
2023-06-09 11:02:46 -0500

    added search results to populate in sidebar

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,103 @@
-
-    //TODO: Add loading animation(message) Remove after load
+//TODO: Add loading animation(message) Remove after load
 
 //TODO: Get all movies from Glitch DB
 
-
-    fetch("https://lava-tranquil-chance.glitch.me/movies"
-    ).then((response) =>
-        response.json().then((favoriteMovies) => {
-            console.log(favoriteMovies);}
-
-    )).catch(err => console.error(err));
-
-
+let getFavorites = () => {
+  fetch("https://lava-tranquil-chance.glitch.me/movies")
+    .then((response) =>
+      response.json().then((favoriteMovies) => {
+        console.log(favoriteMovies);
+      })
+    )
+    .catch((err) => console.error(err));
+};
 
 //TODO: Create a form for user to add favorite movie
+const searchOptions = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTQ3NWZkMTdjMGIyMDQyOTdkODI1M2VhNzBmOTY0MCIsInN1YiI6IjVjNDdjY2JiYzNhMzY4NDc4OTg3Njk0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rVwEd19kyUs8rZsU3h_i-dZk2Xe-qe07Ge6tA0WGMuE",
+  },
+};
 
+// $("#movie-title-input").on("keyup", function () {
+//   let typedWord = document.getElementById("movie-title-input").value;
+//   fetch(
+//     `https://api.themoviedb.org/3/search/movie?query=${typedWord}&include_adult=false&language=en-US&page=1`,
+//     searchOptions
+//   )
+//     .then((movie) => movie.json())
+//     .then((movie) => {
+//       console.log(typedWord);
+//       movie.results.forEach(function (item) {
+//         if (
+//           item.title === typedWord &&
+//           item.poster_path !== null &&
+//           item.vote_count > 1000
+//         ) {
+//           console.log(item.title);
+//         }
+//       });
+//     })
+//     .catch((err) => console.error(err));
+//
+//   // if (movie.title.includes(typedWord)) {
+//   // console.log(movie.resuts.title);
+//   // let posterPath = movie.poster_path;
+//   // let url = `https://image.tmdb.org/t/p/original${posterPath}`;
+//   // let img = $("<img />", {
+//   //   src: url,
+//   //   alt: "Movie poster",
+//   //   class: "movie-posters m-3",
+//   // });
+//   // $("#search-results-list").append(
+//   //   `<li class="movie-list-item">${img}</li>`
+//   // );
+//   // } else {
+//   //   console.log(movie.title);
+//   // }
+// });
 
-    $('#add-movie-button').on('click', function (event) {
-        event.preventDefault()
-        const movie = {
+$("#add-movie-button").on("click", function (event) {
+  event.preventDefault();
+  const movie = {
+    title: document.getElementById("movie-title-input").value,
+  };
+  console.log("Button Works");
+  console.log(movie.title);
+  let movieTitle = movie.title.toLowerCase();
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${movieTitle}&include_adult=false&language=en-US&page=1`,
+    options
+  )
+    .then((movie) => movie.json())
+    .then((movie) => {
+      movie.results.forEach(function (item) {
+        let dBTitle = item.title.toLowerCase();
+        console.log(item.title);
+        if (
+          // movieTitle === dBTitle &&
+          item.poster_path !== null &&
+          item.vote_count > 1000
+        ) {
+          console.log(item.title);
+          let posterPath = item.poster_path;
+          let url = `https://image.tmdb.org/t/p/original${posterPath}`;
+          let img = $("<img />", {
+            src: url,
+            alt: "Movie poster",
+            class: "side-movie-posters m-3",
+          });
+          img.appendTo("#search-results-container");
+        }
+      });
+    })
+    .catch((err) => console.error(err));
+});
 
-            title: document.getElementById('movie-title-input').value,
-            rating: parseInt(document.getElementById('user-movie-rating').value),
-
-        };
-        const url = 'https://lava-tranquil-chance.glitch.me/movies';
-        const favoriteOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(movie),
-        };
-        console.log('Button Works')
-        console.log(movie.title);
-        console.log(movie.rating);
-        fetch(url, favoriteOptions).then(movie => movie.json())
-            .then( movie => console.log(movie) ).then(() => fetch("https://lava-tranquil-chance.glitch.me/movies"
-        ).then((response) =>
-            response.json().then((favoriteMovies) => {
-                console.log(favoriteMovies);}
-
-            )).catch(err => console.error(err))
-            )})
-
-    //TODO: On submit post new movie to Glitch DB, w/ prevent default(stop page reload)
+//TODO: On submit post new movie to Glitch DB, w/ prevent default(stop page reload)
 
 //TODO: Edit an existing movie with  different form
 
@@ -63,54 +116,50 @@
 //TODO: Allow user to search through movies by title, genre, or rating.
 
 //TODO: Connect to TMDB API
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTQ3NWZkMTdjMGIyMDQyOTdkODI1M2VhNzBmOTY0MCIsInN1YiI6IjVjNDdjY2JiYzNhMzY4NDc4OTg3Njk0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rVwEd19kyUs8rZsU3h_i-dZk2Xe-qe07Ge6tA0WGMuE'
-        }
-    };
-    fetch("https://lava-tranquil-chance.glitch.me/movies"
-    ).then((response) =>
-        response.json().then((favoriteMovie) => {
-            favoriteMovie.forEach(
-                function (movie){
-                   let title = movie.title.toLowerCase()
-                    fetch(`https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&page=1`, options)
-                        .then(movie => movie.json())
-                        .then(movie => {
-                            console.log(movie)
-                            movie.results.forEach(function (item) {
-                                let dBTitle = item.title.toLowerCase()
-                                if( title === dBTitle && item.poster_path !== null && item.vote_count > 1000 ) {
-                                    console.log(item.title)
-                                    let posterPath = item.poster_path
-                                    let url = `https://image.tmdb.org/t/p/original${posterPath}`
-                                    var img = $('<img />', {
-                                        src: url,
-                                        alt: 'Movie poster',
-                                        class: 'movie-posters m-3'
-                                    });
-                                    img.appendTo($('#image-container'));
-
-                                }
-
-                            })
-                        })
-                        .catch(err => console.error(err));
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTQ3NWZkMTdjMGIyMDQyOTdkODI1M2VhNzBmOTY0MCIsInN1YiI6IjVjNDdjY2JiYzNhMzY4NDc4OTg3Njk0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rVwEd19kyUs8rZsU3h_i-dZk2Xe-qe07Ge6tA0WGMuE",
+  },
+};
+let renderMovieData = () => {
+  fetch("https://lava-tranquil-chance.glitch.me/movies")
+    .then((response) =>
+      response.json().then((favoriteMovie) => {
+        favoriteMovie.forEach(function (movie) {
+          let title = movie.title.toLowerCase();
+          fetch(
+            `https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&page=1`,
+            options
+          )
+            .then((movie) => movie.json())
+            .then((movie) => {
+              movie.results.forEach(function (item) {
+                let dBTitle = item.title.toLowerCase();
+                if (
+                  title === dBTitle &&
+                  item.poster_path !== null &&
+                  item.vote_count > 1000
+                ) {
+                  console.log(item);
+                  let posterPath = item.poster_path;
+                  let url = `https://image.tmdb.org/t/p/original${posterPath}`;
+                  let img = $("<img />", {
+                    src: url,
+                    alt: "Movie poster",
+                    class: "movie-posters m-3",
+                  });
+                  img.appendTo("#image-container");
                 }
-            )
+              });
+            })
+            .catch((err) => console.error(err));
+        });
+      })
+    )
+    .catch((err) => console.error(err));
+};
 
-        }
-
-        )).catch(err => console.error(err))
-
-
-
-
-
-
-
-
-
-
+renderMovieData();
